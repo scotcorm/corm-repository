8ec94143d2b5226f9cb192d5175969a62195bb3b
Scott Cormier <chicormier@gmail.com>
2024-05-04 14:05:06 -0500

    connect to mongo db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Mongo Database is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
