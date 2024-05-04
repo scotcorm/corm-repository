822581b1880a17fa0f5af45a8b450282045e14f6
Scott Cormier <chicormier@gmail.com>
2024-05-04 12:50:15 -0500

    completed adjustments to header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,10 +13,12 @@ import Projects from './pages/Projects';
 import ShowRecord from './pages/ShowRecord';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
