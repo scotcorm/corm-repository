3b80eadfa820120eab78165b8e6fe17a1ee21a41
Scott Cormier <chicormier@gmail.com>
2024-05-09 14:16:11 -0500

    add delete user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,17 @@
 import express from 'express';
-import { test, updateUser } from '../controllers/user.controller.js';
+import {
+  deleteUser,
+  test,
+  updateUser,
+} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
+// create user route here and in controller
+router.delete('/delete/:userId', verifyToken, deleteUser);
 
 // if token is verified then user is added to the req (at verifyUser.js)then
 // we go to the next function in user.route.js which is updateUser and there
