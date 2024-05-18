d04df7b3b728169d67385b126d3ce98be9fdfae5
Scott Cormier <chicormier@gmail.com>
2024-05-18 16:16:32 -0500

    add admin delete user functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -13,7 +13,7 @@ const router = express.Router();
 
 router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
-// create user route here and in controller
+// create user route here and in controller- user can delete their own profile
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
 router.get('/getusers', verifyToken, getUsers);
