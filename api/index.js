6da1c00436ffb424e0fb9fe4dae86d8d02f7ef01
Scott Cormier <chicormier@gmail.com>
2024-08-04 12:03:07 -0500

    adjusted port binding per Render site

--- api/index.js
+++ api/index.js
@@ -31,11 +31,12 @@ mongoose
 const __dirname = path.resolve();
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is running on port 3000');
 });
 
