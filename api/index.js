fa3a63d4b5788efd9729635f4a66741ccd447e23
Scott Cormier <chicormier@gmail.com>
2024-08-04 12:45:45 -0500

    working on deploying to render- port changes

--- api/index.js
+++ api/index.js
@@ -31,13 +31,13 @@ mongoose
 const __dirname = path.resolve();
 
 const app = express();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
 
 app.use('/api/user', userRoutes);
