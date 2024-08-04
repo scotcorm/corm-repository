a1e75cf3fb5023eca5d0f87fca7fc42516d56336
Scott Cormier <chicormier@gmail.com>
2024-08-04 13:29:09 -0500

    added const port = process.enx.PORT per render docs

--- api/index.js
+++ api/index.js
@@ -31,13 +31,14 @@ mongoose
 const __dirname = path.resolve();
 
 const app = express();
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(port, () => {
+  // console.log('Server is running on port 3000');
+  console.log(`Server is running on port ${port}`);
 });
 
 app.use('/api/user', userRoutes);
